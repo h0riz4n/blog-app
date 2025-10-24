6f120c63e576b4a8519d5fde9332e6047c6c2bee
Rustam Khudoyan <dreamang2toplay@yandex.ru>
2025-10-24 23:43:20 +0300

    feat: Обновлён обработчик ошибок

--- src/main/java/ru/yandex/blog_app/handler/RestControllerHandler.java
+++ src/main/java/ru/yandex/blog_app/handler/RestControllerHandler.java
@@ -1,11 +1,17 @@
 package ru.yandex.blog_app.handler;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -23,6 +29,21 @@ public class RestControllerHandler extends ResponseEntityExceptionHandler  {
         return ResponseEntity
             .status(ex.getStatus())
             .body(buildProblemDetail(webRequest, ex, ex.getStatus()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest webRequest) {
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> errorMap.put(((FieldError) error).getField(), error.getDefaultMessage()));
+        ProblemDetail body = buildProblemDetail(webRequest, ex, HttpStatus.BAD_REQUEST, Map.of("validation", errorMap));
+        return this.handleExceptionInternal(ex, body, headers, status, webRequest);
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        log.debug(ex.getMessage());
+        return super.handleExceptionInternal(ex, body, headers, statusCode, request);
     }
 
     private ProblemDetail buildProblemDetail(WebRequest webRequest, Exception ex, HttpStatus httpStatus) {
